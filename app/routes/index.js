6d85479793ebcc6316f272e16fc56ce668d9798d
Chace Kim <andy0711@gmail.com>
2015-12-03 02:18:25 +0000

    API for led control

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Gpio = require('onoff');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +9,16 @@ router.get('/', function(req, res, next) {
 
 router.get('/led', function(req, res, next) {
   res.render('led', { myVar: 'Yo' });
+});
+
+router.get('/led/on', function(req, res, next) {
+  console.log('LED is turned on');
+  led.writeSync(1);
+});
+
+router.get('/led/off/', function(req, res, next) {
+  console.log('LED is turned OFF');
+  led.writeSync(0);
 });
 
 module.exports = router;
