967d6e1cb3a78e10e07d326da90ebd8ba2989b51
Chace Kim <andy0711@gmail.com>
2015-12-05 12:07:15 +1300

    minor fix

--- app/routes/led.js
+++ app/routes/led.js
@@ -4,17 +4,21 @@ var pin = require('../pin');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/led', function(req, res, next) {
   console.log('This is where you can control LED with Web Interface');
   res.render('led', { title: 'Express' });
 });
 
-router.get('/on', function(req, res, next) {
+router.get('/led/on', function(req, res, next) {
   console.log('LED is turned ON');
   pin.led.writeSync(1);
   res.render('on');
 });
 
-router.get('/off', function(req, res, next) {
+router.get('/led/off', function(req, res, next) {
   console.log('LED is turned OFF');
   pin.led.writeSync(0);
   res.render('off');
