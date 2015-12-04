03131aec7dac4f4e134dc6a270a8e7942e3a4995
Chace Kim <andy0711@gmail.com>
2015-12-05 11:58:49 +1300

    jade to ejs

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var Gpio = require('onoff').Gpio;
 
-var routes = require('./routes/index');
+var routes = require('./routes/led');
 var users = require('./routes/users');
 
 var app = express();
