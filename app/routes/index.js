037bdbc60def20d0652b9d8f385b38c4d55b9939
Chace Kim <andy0711@gmail.com>
2015-12-04 16:30:55 +1300

    pin.js created

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Gpio = require('onoff');
+var pin = require('./pin');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,12 +14,12 @@ router.get('/led', function(req, res, next) {
 
 router.get('/led/on', function(req, res, next) {
   console.log('LED is turned on');
-  led.writeSync(1);
+  pin.led.writeSync(1);
 });
 
 router.get('/led/off/', function(req, res, next) {
   console.log('LED is turned OFF');
-  led.writeSync(0);
+  pin.led.writeSync(0);
 });
 
 module.exports = router;
